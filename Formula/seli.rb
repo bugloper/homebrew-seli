74fbd227b56724812413ff22048c0e245d0f5ed3
bugloper <bugloper@hey.com>
2026-04-06 19:14:52 +0600

    0.2.2

--- Formula/seli.rb
+++ Formula/seli.rb
@@ -5,27 +5,27 @@ class Seli < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/bugloper/homebrew-seli/releases/download/0.2.1/seli_0.2.1_darwin_amd64.tar.gz"
-    sha256 "b456a06f0c74e400011afc973e0483d20a5ce90f4065da30f809e8a622ae2e28"
+    sha256 "8d31b1e907ff8c805d710455ce5f35a4848acd69486f33b5da77267c72cd0f88"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/bugloper/homebrew-seli/releases/download/0.2.1/seli_0.2.1_darwin_arm64.tar.gz"
-    sha256 "1030234056b2678b23f40883dad7715a4b31e0667aac6cb84fa81936fa4dd521"
+    sha256 "31df09f77a4a84a6478161ec91daa3f8c70fb1f38ffd8e6cc67c3ccb4a2ad938"
   end
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/bugloper/homebrew-seli/releases/download/0.2.1/seli_0.2.1_linux_amd64.tar.gz"
-    sha256 "dc99be3a1c32e91d1238810ed6666edfb8b7395f0055589dd04c570bc7b60ff4"
+    sha256 "a815ee1d67d775320af16c4dcb4734a84281973b17fba97ddf42679efd9cad88"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
     url "https://github.com/bugloper/homebrew-seli/releases/download/0.2.1/seli_0.2.1_linux_arm.tar.gz"
-    sha256 "f38749bfbc3e7eb31ca71ed19352d3d23fbcaa421bced719f7dd1860fe35759e"
+    sha256 "b6ce77885608f86003d6bad2ab8bcc7c84c6215008adc6e417ef85b3a70b2ec3"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/bugloper/homebrew-seli/releases/download/0.2.1/seli_0.2.1_linux_arm64.tar.gz"
-    sha256 "86a8bdb30ff86b45e1ce1df3d0c2f2992d4a2df4fa9d75ffe43173b412813a55"
+    sha256 "5ea31b2b8ac8e325f0929bb1a76cc1124031130f4e0bf7013cf47d51b8048844"
   end
 
   def install
